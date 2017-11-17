967527367fefc3aeb263c31b5bb6050ae9a2e7e9
Kingwalla <kingwalla@aim.com>
2017-11-17 08:52:46 -0500

    This is First Commit

---
+++ Amortization.py
@@ -0,0 +1,10 @@
+import pandas
+import numpy as np
+from datetime import date
+#Define variables below for specific property
+Interest_Rate = .04
+Years = 30
+Payments_Year = 12
+Principal = 200000
+Addl_Princ = 50
+start_date = (date(2016,1,1))
