b33ed3cfa4fa8b3c3a96076fb492f7bb728ec182
Kingwalla <kingwalla@aim.com>
2017-11-17 09:08:48 -0500

    Second Iteration

--- Amortization.py
+++ Amortization.py
@@ -8,3 +8,12 @@ Payments_Year = 12
 Principal = 200000
 Addl_Princ = 50
 start_date = (date(2016,1,1))
+#Function below
+pmt = np.pmt(Interest_Rate/Payments_Year, Years*Payments_Year, Principal)
+print pmt
+
+per = 1
+ipmt = np.ipmt(Interest_Rate/Payments_Year, per, Years*Payments_Year, Principal)
+ppmt = np.ppmt(Interest_Rate/Payments_Year, per, Years*Payments_Year, Principal)
+print(ipmt, ppmt)
+
